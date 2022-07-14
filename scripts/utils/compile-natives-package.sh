f7fe54e1e074206bdc6d8383252715c2d8377990
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-07-14 21:21:38 +0300

    it works!

--- scripts/utils/compile-natives-package.sh
+++ scripts/utils/compile-natives-package.sh
@@ -2,14 +2,25 @@
 # This script compiles tdlight/tdlib jni binaries for your platform.
 # Fill the variables with your system details.
 #
+
+# copy paste src/main/replacements/replace-osx-amd64-tdlight-legacy.sed
+# and src/main/replacements/replace-osx-amd64-tdlight-sealed.sed
+# to src/main/replacements/replace-osx-aarch64-tdlight-legacy.sed and
+# src/main/replacements/replace-osx-aarch64-tdlight-sealed.sed
+# (and replace "amd64" with "aarch64" inside)
+
 # MAIN REQUIRED ENVIRONMENT VARIABLES:
-#   OPERATING_SYSTEM_NAME = <windows | linux | osx>
-#   CPU_ARCHITECTURE_NAME = <amd64 | aarch64 | 386 | armhf | ppc64le>
-#   OPERATING_SYSTEM_NAME_SHORT = <win | linux | osx>
-#   IMPLEMENTATION_NAME = <tdlib | tdlight>
-#   CPU_CORES = "-- -j<cores>" or "-m" on Windows
-#   CMAKE_EXTRA_ARGUMENTS = <args>
-#   REVISION = <revision>
+brew install maven
+brew install openssl
+brew install coreutils
+export OPENSSL_ROOT_DIR="/opt/homebrew/opt/openssl@3"
+export OPERATING_SYSTEM_NAME="osx"
+export CPU_ARCHITECTURE_NAME="aarch64"
+export OPERATING_SYSTEM_NAME_SHORT="osx"
+export IMPLEMENTATION_NAME="tdlight"
+export CPU_CORES="-- -j8"
+export REVISION=1
+export BUILD_TYPE="Release"
 # OPTIONAL ENVIRONMENT VARIABLES:
 #   CROSS_BUILD_DEPS_DIR = <args>
 
